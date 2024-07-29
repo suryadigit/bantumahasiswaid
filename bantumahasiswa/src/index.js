8d200a1ee77982b59bd91d83546605b76c1076b9
suryadigit <211111397@students.mikroskil.ac.id>
2024-07-30 03:18:48 +0700

    chore: Update import statement for Dashboard component

--- bantumahasiswa/src/index.js
+++ bantumahasiswa/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Dashboard from './dashboard.js';
+import Dashboard from './Dashboard.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

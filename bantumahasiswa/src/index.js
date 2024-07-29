378fd1cd11d9dd5322cd2985e826a4a32a78fea7
suryadigit <211111397@students.mikroskil.ac.id>
2024-07-30 03:19:58 +0700

    chore: Update import statement for Dashboard component
    The import statement for the Dashboard component in the index.js file has been updated to use the correct file name. This change ensures that the Dashboard component is properly imported and rendered in the Bantu Mahasiswa application.

--- bantumahasiswa/src/index.js
+++ bantumahasiswa/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Dashboard from './Dashboard.js';
-
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
